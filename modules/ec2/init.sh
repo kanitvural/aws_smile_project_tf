584e6959b7ee6d2fb646d21cec450218b1d6e6d4
kntvrl <kanitvural@gmail.com>
2024-08-03 06:07:38 +0300

    update init

--- modules/ec2/init.sh
+++ modules/ec2/init.sh
@@ -9,12 +9,51 @@ sudo systemctl enable nginx
 sudo systemctl start nginx
 git clone https://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@github.com/kntvrl/aws_smile_project_tf.git /home/ec2-user/aws_smile_project_tf
 
-chmod +x /home/ec2-user/aws_smile_project_tf/setup_env.sh
-chmod +x /home/ec2-user/aws_smile_project_tf/setup_service.sh
+
+# service kurulumu
+sudo tee /etc/systemd/system/smile.service > /dev/null <<EOL
+[Unit]
+Description=Smile Python Application
+After=network.target
+
+[Service]
+User=ec2-user
+Group=ec2-user
+WorkingDirectory=/home/ec2-user/aws_smile_project_tf/
+ExecStart=/home/ec2-user/aws_smile_project_tf/venv/bin/python3 /home/ec2-user/aws_smile_project_tf/smile_app.py
+Restart=always
+RestartSec=5
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+sudo systemctl daemon-reload
+sudo systemctl start smile.service
+sudo systemctl enable smile.service
 
 
-# Ortamı ayarla
-bash /home/ec2-user/aws_smile_project_tf/setup_env.sh
+# ortama paket yükleme
 
-# Servisi ayarla
-bash /home/ec2-user/aws_smile_project_tf/setup_service.sh
+cd /home/ec2-user/aws_smile_project_tf
+python3.11 -m venv venv
+
+if [ $? -ne 0 ]; then
+    echo "Venv could not be created"
+    exit 1
+fi
+
+
+source venv/bin/activate
+pip install -r requirements.txt
+
+if [ $? -ne 0 ]; then
+    echo "Package initialization work failed!"
+    exit 1
+fi
+
+
+
+
+
+
