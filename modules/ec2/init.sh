026bc7d634ef40eb875bb7763bc6ce016cbd991c
kntvrl <kanitvural@gmail.com>
2024-08-03 09:32:15 +0300

    up

--- modules/ec2/init.sh
+++ modules/ec2/init.sh
@@ -7,20 +7,16 @@ sudo dnf install git python3.11 python3.11-pip nginx -y
 
 sleep 20
 
-sudo cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.original
-sudo cp /home/ec2-user/aws_smile_project_tf/nginx.conf /etc/nginx/nginx.conf
-
-
-
-sudo systemctl enable nginx
-sudo systemctl start nginx
-
-sleep 10
-
 # project download
 git clone https://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@github.com/kntvrl/aws_smile_project_tf.git /home/ec2-user/aws_smile_project_tf
 
-sleep 5
+sleep 10
+
+#nginx setup
+sudo cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.original
+sudo cp /home/ec2-user/aws_smile_project_tf/nginx.conf /etc/nginx/nginx.conf
+sudo systemctl enable nginx
+sudo systemctl start nginx
 
 # service setup
 sudo tee /etc/systemd/system/smile.service > /dev/null <<EOL
@@ -43,8 +39,6 @@ EOL
 sudo systemctl daemon-reload
 sudo systemctl start smile.service
 sudo systemctl enable smile.service
-
-
 
 
 # setup venv
